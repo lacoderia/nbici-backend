57a898756dc3e07ebeda48ac7a98e62aae171875
tonklis <benjamin.hernandez@in2teck.com>
2016-06-14 21:17:04 -0500

    Fixed migration

--- db/migrate/20160613215907_add_expiration_date_to_users.rb
+++ db/migrate/20160613215907_add_expiration_date_to_users.rb
@@ -1,6 +1,17 @@
 class AddExpirationDateToUsers < ActiveRecord::Migration
   def change
     add_column :users, :expiration_date, :datetime
-    User.create_expiration_date_for_users_with_purchases
+    
+    users_with_purchases = User.joins(:purchases)
+    users_with_purchases.each do |user|
+      day_count = 0
+      user.purchases.each do |purchase|
+        day_count += purchase.pack.expiration
+      end
+      
+      user.expiration_date = user.last_class_purchased + day_count.days
+      user.save!
+    end
+
   end
 end
