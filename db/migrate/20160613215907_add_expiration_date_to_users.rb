6355758f87f4e56bad3326f1a2ae29cc0f70ada5
tonklis <benjamin.hernandez@in2teck.com>
2016-06-14 21:26:33 -0500

    Leaving Migration to be executed manually

--- db/migrate/20160613215907_add_expiration_date_to_users.rb
+++ db/migrate/20160613215907_add_expiration_date_to_users.rb
@@ -1,10 +1,5 @@
 class AddExpirationDateToUsers < ActiveRecord::Migration
-  def up
+  def change
     add_column :users, :expiration_date, :datetime
-    User.create_expiration_date_for_users_with_purchases
-  end
-
-  def down
-    remove_column :users, :expiration_date
   end
 end
