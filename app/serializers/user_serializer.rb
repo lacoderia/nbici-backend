693789d5f4c758a41c18596c71ac32ca9a1103fc
tonklis <benjamin.hernandez@in2teck.com>
2016-06-29 16:59:08 -0500

    Added coupon_value to session

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :classes_left, :last_class_purchased, :picture, :uid, :active, :phone, :coupon, :credits
+  attributes :id, :first_name, :last_name, :email, :classes_left, :last_class_purchased, :picture, :uid, :active, :phone, :coupon, :coupon_value, :credits
+
+  def coupon_value
+    Configuration.coupon_discount
+  end
 end
