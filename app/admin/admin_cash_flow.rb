556870b85c0ee025b5af8fd44cb76987e451a839
tonklis <benjamin.hernandez@in2teck.com>
2016-07-05 14:17:37 -0500

    Added CSV support

--- app/admin/admin_cash_flow.rb
+++ app/admin/admin_cash_flow.rb
@@ -47,5 +47,38 @@ ActiveAdmin.register Purchase, :as => "Control_de_ingresos" do
       end
     end
   end
+
+  csv do
+
+    column "Cliente" do |purchase|
+      "#{purchase.user.first_name} #{purchase.user.last_name}"
+    end
+
+    column "Paquete" do |purchase|
+      "#{purchase.pack.description}"
+    end
+
+    column "Precio" do |purchase|
+      purchase.amount / 100.0
+    end
+
+    column "Fecha", :created_at
+
+    column "Usadas" do |purchase|
+      appointments_in_month = purchase.user.appointments.where("start BETWEEN ? and ?", purchase.bom, purchase.eom)
+      appointments_in_month.count
+    end
+
+    column "Disponible" do |purchase|
+      appointments_in_month = purchase.user.appointments.where("start BETWEEN ? and ?", purchase.bom, purchase.eom)
+      price_per_class = (purchase.amount / 100.0) / purchase.pack.classes
+      if appointments_in_month.count <= purchase.pack.classes
+        appointments_in_month.count * price_per_class
+      else
+        0.0
+      end
+    end
+    
+  end
   
 end
