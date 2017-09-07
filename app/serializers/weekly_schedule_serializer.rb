fb07e983faaa257dfe3b1f31955dcda51e147f8d
tonklis <benjamin.hernandez@in2teck.com>
2017-09-07 12:45:13 -0500

    Fixed instructor nested item in weekly_schedule_serializer.

--- app/serializers/weekly_schedule_serializer.rb
+++ app/serializers/weekly_schedule_serializer.rb
@@ -3,7 +3,7 @@ module WeeklyScheduleSerializer
   def WeeklyScheduleSerializer.serialize(schedules_with_start_date)
 
     result = {}
-    result[:schedules] = ActiveModel::ArraySerializer.new(schedules_with_start_date[:schedules], each_serializer: ScheduleSerializer, except: :instructor) 
+    result[:schedules] = ActiveModel::ArraySerializer.new(schedules_with_start_date[:schedules], each_serializer: ScheduleSerializer) 
     result[:start_day] = schedules_with_start_date[:start_day]
     result
   end
