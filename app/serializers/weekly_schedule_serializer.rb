e0b9b3f56f4b34a05611a7b8fe382406d54e1b93
tonklis <benjamin.hernandez@in2teck.com>
2017-09-08 08:34:34 -0500

    added check to include instructor in serialized object

--- app/serializers/weekly_schedule_serializer.rb
+++ app/serializers/weekly_schedule_serializer.rb
@@ -1,9 +1,13 @@
 module WeeklyScheduleSerializer
 
-  def WeeklyScheduleSerializer.serialize(schedules_with_start_date)
+  def WeeklyScheduleSerializer.serialize(schedules_with_start_date, with_instructor = false)
 
     result = {}
-    result[:schedules] = ActiveModel::ArraySerializer.new(schedules_with_start_date[:schedules], each_serializer: ScheduleSerializer) 
+    if with_instructor
+      result[:schedules] = ActiveModel::ArraySerializer.new(schedules_with_start_date[:schedules], each_serializer: ScheduleSerializer) 
+    else
+      result[:schedules] = ActiveModel::ArraySerializer.new(schedules_with_start_date[:schedules], each_serializer: ScheduleSerializer, except: :instructor) 
+    end
     result[:start_day] = schedules_with_start_date[:start_day]
     result
   end
