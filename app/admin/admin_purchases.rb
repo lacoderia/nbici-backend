b104bde785d346abd1d2ea64a5349a782f42dc94
tonklis <benjamin.hernandez@in2teck.com>
2019-12-09 15:08:53 -0600

    Added begin rescue to admin purchases

--- app/admin/admin_purchases.rb
+++ app/admin/admin_purchases.rb
@@ -21,99 +21,113 @@ ActiveAdmin.register Purchase, :as => "Compras" do
 
     def create
 
+      begin
+
       # Make purchase
-      if params[:purchase][:user_attributes] && params[:purchase][:user_attributes][:card_ids]
+        if params[:purchase][:user_attributes] && params[:purchase][:user_attributes][:card_ids]
 
-        user = User.find(params[:purchase][:user_attributes][:id])
-        amount_initial = params[:purchase][:amount]
-        amount_confirmation = params[:purchase][:amount_confirmation]
-        currency = "MXN"
-        description = params[:purchase][:description]
+          user = User.find(params[:purchase][:user_attributes][:id])
+          amount_initial = params[:purchase][:amount]
+          amount_confirmation = params[:purchase][:amount_confirmation]
+          currency = "MXN"
+          description = params[:purchase][:description]
+            
+          card = Card.find(params[:purchase][:user_attributes][:card_ids])
+
+          # render error        
+          if (amount_initial != amount_confirmation) || (amount_initial.blank?) || (amount_initial.to_f < 3)
+            flash[:error] = 'La cantidad no es válida'
+            redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
           
-        card = Card.find(params[:purchase][:user_attributes][:card_ids])
-
-        # render error        
-        if (amount_initial != amount_confirmation) || (amount_initial.blank?) || (amount_initial.to_f < 3)
-          flash[:error] = 'La cantidad no es válida'
-          redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
-        
-        elsif description.blank?
-          flash[:error] = 'Se necesita una descripción'
-          redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
-        else
-
-          amount = (amount_initial.to_f * 100).to_i
-
-          if not user.test?
-
-            charge = Conekta::Charge.create({
-              amount: amount,
-              currency: currency,
-              description: description,
-              card: card.uid,
-              details: {
-                name: card.name,
-                email: user.email,
-                phone: card.phone,
-                line_items: [{
-                  name: description,
-                  description: "Compra en recepcion",
-                  unit_price: amount,
-                  quantity: 1
-                }]
-              }
-            })
-
-            purchase = Purchase.create!(
-              user: user,
-              pack: nil,
-              uid: charge.id,
-              object: charge.object, 
-              livemode: charge.livemode, 
-              status: charge.status,
-              description: charge.description,
-              amount: charge.amount,
-              currency: charge.currency,
-              payment_method: charge.payment_method,
-              details: charge.details,
-              promotion: nil
-            ) 
-    
+          elsif description.blank?
+            flash[:error] = 'Se necesita una descripción'
+            redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
           else
 
-            purchase = Purchase.create!(
-              user: user,
-              pack: nil,
-              uid: nil,
-              object: nil, 
-              livemode: nil, 
-              status: nil,
-              description: description,
-              amount: amount,
-              currency: currency,
-              payment_method: nil,
-              details: nil,
-              promotion: nil
-            )
+            amount = (amount_initial.to_f * 100).to_i
 
-          end        
-        
-          #NbiciMailer.send(:front_desk_purchase, user, purchase).deliver_now
-          SendEmailJob.perform_later("front_desk_purchase", user, purchase)
+            if not user.test?
+
+              charge = Conekta::Charge.create({
+                amount: amount,
+                currency: currency,
+                description: description,
+                card: card.uid,
+                details: {
+                  name: card.name,
+                  email: user.email,
+                  phone: card.phone,
+                  line_items: [{
+                    name: description,
+                    description: "Compra en recepcion",
+                    unit_price: amount,
+                    quantity: 1
+                  }]
+                }
+              })
+
+              purchase = Purchase.create!(
+                user: user,
+                pack: nil,
+                uid: charge.id,
+                object: charge.object, 
+                livemode: charge.livemode, 
+                status: charge.status,
+                description: charge.description,
+                amount: charge.amount,
+                currency: charge.currency,
+                payment_method: charge.payment_method,
+                details: charge.details,
+                promotion: nil
+              ) 
       
-          flash[:notice] = 'La compra se realizó correctamente.'
-          redirect_to collection_url
+            else
+
+              purchase = Purchase.create!(
+                user: user,
+                pack: nil,
+                uid: nil,
+                object: nil, 
+                livemode: nil, 
+                status: nil,
+                description: description,
+                amount: amount,
+                currency: currency,
+                payment_method: nil,
+                details: nil,
+                promotion: nil
+              )
+
+            end        
+          
+            #NbiciMailer.send(:front_desk_purchase, user, purchase).deliver_now
+            SendEmailJob.perform_later("front_desk_purchase", user, purchase)
+        
+            flash[:notice] = 'La compra se realizó correctamente.'
+            redirect_to collection_url
+
+          end
+
+        # render error
+        else
+          
+          flash[:error] = 'Información incompleta'
+          redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
 
         end
 
-      # render error
-      else
-        
-        flash[:error] = 'Información incompleta'
-        redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
+      rescue Exception => e
 
-      end
-      
+        if e.try(:message_to_purchaser)
+          error = e.message_to_purchaser
+        else
+          error = e.message
+        end
+        
+        flash[:error] = error
+        redirect_to "/admin/compras/new?user_id=#{params[:purchase][:user_attributes][:id]}" 
+        
+      end      
 
     end
     
